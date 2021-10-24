4154a4029f7f22b74e6947ac3517092267ece369
Daiki Oshima <73574599+Daiki-Oshima@users.noreply.github.com>
2021-10-24 10:58:47 +0900

    Add files via upload

--- js/App.js
+++ js/App.js
@@ -1,1 +1,7 @@
-
+import skillset from "./modules/skillset";
+import cursor from "./modules/cursor";
+import letter from "./modules/jquery.lettering";
+import mainvisual from "./modules/mainvisual";
+import drawer from "./modules/drawerMenu";
+import modal from "./modules/modal";
+import smoothScroll from "./modules/smoothScroll";
