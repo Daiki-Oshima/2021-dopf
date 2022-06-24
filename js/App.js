ab58a343ffa8bc1005284dc8867e525fbdf612af
oshima-daiki <daikougun0923@gmail.com>
2022-06-24 16:51:39 +0900

    updata

--- js/App.js
+++
@@ -1,7 +0,0 @@
-import skillset from "./modules/skillset";
-import cursor from "./modules/cursor";
-import letter from "./modules/jquery.lettering";
-import mainvisual from "./modules/mainvisual";
-import drawer from "./modules/drawerMenu";
-import modal from "./modules/modal";
-import smoothScroll from "./modules/smoothScroll";
